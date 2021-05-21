9a727f36864ee41a8ba27f3955534b61bd88ad46
Granaino <Granaino@LAPTOP-I75TB97R>
2021-05-21 19:12:10 +0200

    Clase que halla la suma y la resta de tres números

---
+++ ProyectoGit/src/Ejemplo.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//Definimos las variables
+		int n1;		
+		int n2;		
+		int n3;
+		
+		int suma;
+		int resta;
+		
+		char continuar='S';
+		
+//Para leer creamos el objeto teclado
+		Scanner teclado = new Scanner(System.in);
+		
+//Mientras que quiera continuar:
+		while(continuar=='S' | continuar=='s') {
+			
+//Introducimos las variables
+			System.out.print("Introduce el valor del primer numero:");
+			n1 = teclado.nextInt();
+
+			System.out.print("Introduce el valor del segundo numero:");
+			n2 = teclado.nextInt();
+
+			System.out.print("Introduce el valor del tercer numero:");
+			n3 = teclado.nextInt();
+			
+//Hallamos la suma, la resta y la division
+			suma = n1 + n2 + n3;
+			
+			resta = n1 - n2 - n3;
+			
+			System.out.println("La suma es " + suma + " y la resta es " + resta);
+			
+			System.out.print("Quieres continuar?(S/N)");
+			continuar=teclado.next().charAt(0);
+		}
+		
+		teclado.close();
+	}
+
+}
